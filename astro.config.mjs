fd5b2fdc3ff18a989f331110a3e3a7e809718920
Cédric Bontems <67456035+cbontems@users.noreply.github.com>
2023-06-05 21:52:51 +0200

    feat: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://astroprops.netlify.app/",
-	integrations: [robotsTxt()],
+	integrations: [robotsTxt(), sitemap()],
 });
